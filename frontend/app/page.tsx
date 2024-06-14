5c5fdd4265c72d4cc6cea559db892d7d129309c2
Daniel Leavitt <daniel.7039@hotmail.com>
2024-06-14 09:39:00 -0700

    card grid

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -27,6 +27,34 @@ import {
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 // import { track } from "@vercel/analytics";
 
+const dummyProjects = [
+  {
+    name: "Project 1",
+    description: "This is a description of project 1",
+    votes: 0,
+  },
+  {
+    name: "Project 2",
+    description: "This is a description of project 2",
+    votes: 0,
+  },
+  {
+    name: "Project 3",
+    description: "This is a description of project 3",
+    votes: 0,
+  },
+  {
+    name: "Project 4",
+    description: "This is a description of project 4",
+    votes: 0,
+  },
+  {
+    name: "Project 5",
+    description: "This is a description of project 5",
+    votes: 0,
+  },
+];
+
 export default function Page() {
   const client = useSuiClient(); // The SuiClient instance
   const enokiFlow = useEnokiFlow(); // The EnokiFlow instance
@@ -36,45 +64,17 @@ export default function Page() {
   const [balance, setBalance] = useState<number>(0);
   const [accountLoading, setAccountLoading] = useState<boolean>(true);
 
-  /* Transfer form state */
-  const [recipientAddress, setRecipientAddress] = useState<string>("");
-  const [amount, setAmount] = useState<string>("");
-  const [transferLoading, setTransferLoading] = useState<boolean>(false);
-
-  const [burnAmount, setBurnAmount] = useState<string>("");
-  const [burnLoading, setBurnLoading] = useState<boolean>(false);
-
-  /* Counter state */
-  const [counter, setCounter] = useState<number>(0);
-  const [counterLoading, setCounterLoading] = useState<boolean>(false);
-  const [countLoading, setCountLoading] = useState<boolean>(true);
-
-  /**
-   * Timeout for the counter.
-   * This is used to refresh the counter every 5 seconds.
-   */
-  useEffect(() => {
-    const interval = setInterval(() => {
-      getCount();
-    }, 5000);
-    
-    return () => clearInterval(interval);
-  }, []);
-
   /**
    * When the user logs in, fetch the account information.
    */
   useEffect(() => {
     if (suiAddress) {
       getAccountInfo();
-      getCount();
     }
   }, [suiAddress]);
 
   const startLogin = async () => {
     const promise = async () => {
-      console.log("Starting login flow...");
-      console.log("Google client id", process.env.GOOGLE_CLIENT_ID!);
       
       window.location.href = await enokiFlow.createAuthorizationURL({
         provider: "google",
@@ -157,262 +157,10 @@ export default function Page() {
     });
   };
 
-  /**
-   * Transfer SUI to another account. This transaction is not sponsored by the app.
-   */
-  async function transferSui() {
-    const promise = async () => {
-      //track("Transfer SUI");
-
-      setTransferLoading(true);
-
-      // Validate the transfer amount
-      const parsedAmount = parseFloat(amount);
-      if (isNaN(parsedAmount)) {
-        setTransferLoading(false);
-        throw new Error("Invalid amount");
-      }
-
-      // Get the keypair for the current user.
-      const keypair = await enokiFlow.getKeypair({ network: "testnet" });
-
-      // Create a new transaction block
-      const txb = new Transaction();
-
-      // Add some transactions to the block...
-      const [coin] = txb.splitCoins(txb.gas, [
-        txb.pure.u64(parsedAmount * 10 ** 9),
-      ]);
-      txb.transferObjects([coin], txb.pure.address(recipientAddress));
-
-      // Sign and execute the transaction block, using the Enoki keypair
-      const res = await client.signAndExecuteTransaction({
-        signer: keypair,
-        transaction: txb,
-        options: {
-          showEffects: true,
-          showBalanceChanges: true,
-        },
-      });
-
-      setTransferLoading(false);
-
-      console.log("Transfer response", res);
-
-      if (res.effects?.status.status !== "success") {
-        const suiBalanceChange =
-          res.balanceChanges
-            ?.filter((balanceChange: BalanceChange) => {
-              return balanceChange.coinType === "0x2::sui::SUI";
-            })
-            .map((balanceChange: BalanceChange) => {
-              return parseInt(balanceChange.amount) / 10 ** 9;
-            })
-            .reduce((acc: number, change: any) => {
-              if (change.coinType === "0x2::sui::SUI") {
-                return acc + parseInt(change.amount);
-              }
-              return acc;
-            }) || 0;
-        setBalance(balance - suiBalanceChange);
-        throw new Error(
-          "Transfer failed with status: " + res.effects?.status.error
-        );
-      }
-
-      return res;
-    };
-
-    toast.promise(promise, {
-      loading: "Transfer SUI...",
-      success: (data) => {
-        const suiBalanceChange =
-          data.balanceChanges
-            ?.filter((balanceChange: BalanceChange) => {
-              return balanceChange.coinType === "0x2::sui::SUI";
-            })
-            .map((balanceChange: BalanceChange) => {
-              return parseInt(balanceChange.amount) / 10 ** 9;
-            })
-            .reduce((acc: number, change: any) => {
-              if (change.coinType === "0x2::sui::SUI") {
-                return acc + parseInt(change.amount);
-              }
-              return acc;
-            }) || 0;
-        setBalance(balance - suiBalanceChange);
-
-        return (
-          <span className="flex flex-row items-center gap-2">
-            Transfer successful!{" "}
-            <a
-              href={`https://suiscan.xyz/testnet/tx/${data.digest}`}
-              target="_blank"
-            >
-              <ExternalLink width={12} />
-            </a>
-          </span>
-        );
-      },
-      error: (error) => {
-        return error.message;
-      },
-    });
-  }
-
-  async function burnSui() {
-    const promise = async () => {
-      setBurnLoading(true);
-
-      const parseBurnAmount = parseFloat(burnAmount);
-
-      // Get the keypair for the current user.
-      const keypair = await enokiFlow.getKeypair({ network: "testnet" });
-
-      // Create a new transaction block
-      const txb = new Transaction();
-
-      const [coin] = txb.splitCoins(txb.gas, [txb.pure.u64(parseBurnAmount * 10 ** 9)]);
-
-      // Add some transactions to the block...
-      txb.moveCall({
-        arguments: [
-          coin, 
-          txb.object(
-            "0x63da96be626a3657bd65d925ab6fb2e6cb29b2c4e0c3727e4d034f50d5c3110a"
-          )
-        ],
-        target:
-          "0x2ff8b515c11d8b3a3f7ee134b9ebd8f2373350b5aaf26924e94ff0bce224e198::burner::deposit",
-      });
-
-      try {
-        // Sponsor and execute the transaction block, using the Enoki keypair
-        const res = await enokiFlow.sponsorAndExecuteTransaction({
-          transaction: txb,
-          network: "testnet",
-          client,
-        });
-        // const res = await client.signAndExecuteTransaction({
-        //   signer: keypair,
-        //   transaction: txb,
-        //   options: {
-        //     showEffects: true,
-        //     showBalanceChanges: true,
-        //   },
-        // });
-        setBurnLoading(false);
-
-        return res;
-      } catch (error) {
-        setBurnLoading(false);
-        throw error;
-      }
-    };
-
-    toast.promise(promise, {
-      loading: "Incrementing counter...",
-      success: (data) => {
-        return (
-          <span className="flex flex-row items-center gap-2">
-            Burned
-            <a
-              href={`https://suiscan.xyz/testnet/tx/${data.digest}`}
-              target="_blank"
-            >
-              <ExternalLink width={12} />
-            </a>
-          </span>
-        );
-      },
-      error: (error) => {
-        console.error(error);
-        return error.message;
-      },
-    });
-  }
-
-  async function getCount() {
-    const res = (await client.getObject({
-      id: "0xd710735500fc1be7dc448b783ad1fb0b5fd209890a67e518cc47e7dc26856aa6",
-      options: {
-        showContent: true,
-      },
-    })) as any;
-
-    setCounter(res.data.content.fields.count as number);
-
-    setCountLoading(false);
-  }
-
-  /**
-   * Increment the global counter. This transaction is sponsored by the app.
-   */
-  async function incrementCounter() {
-    const promise = async () => {
-      //track("Increment Counter");
-
-      setCounterLoading(true);
-
-      // Get the keypair for the current user.
-      const keypair = await enokiFlow.getKeypair({ network: "testnet" });
-
-      // Create a new transaction block
-      const txb = new Transaction();
-
-      // Add some transactions to the block...
-      txb.moveCall({
-        arguments: [
-          txb.object(
-            "0xd710735500fc1be7dc448b783ad1fb0b5fd209890a67e518cc47e7dc26856aa6"
-          ),
-        ],
-        target:
-          "0x5794fff859ee70e28ec8a419f2a73830fb66bcaaaf76a68e41fcaf5e057d7bcc::global_counter::increment",
-      });
-
-      try {
-        // Sponsor and execute the transaction block, using the Enoki keypair
-        const res = await enokiFlow.sponsorAndExecuteTransaction({
-          transaction: txb,
-          network: "testnet",
-          client,
-        });
-        setCounterLoading(false);
-
-        return res;
-      } catch (error) {
-        setCounterLoading(false);
-        throw error;
-      }
-    };
-
-    toast.promise(promise, {
-      loading: "Incrementing counter...",
-      success: (data) => {
-        getCount();
-        return (
-          <span className="flex flex-row items-center gap-2">
-            Counter incremented!{" "}
-            <a
-              href={`https://suiscan.xyz/testnet/tx/${data.digest}`}
-              target="_blank"
-            >
-              <ExternalLink width={12} />
-            </a>
-          </span>
-        );
-      },
-      error: (error) => {
-        return error.message;
-      },
-    });
-  }
-
   if (suiAddress) {
     return (
       <div>
-        <h1 className="text-4xl font-bold m-4">Enoki Demo App</h1>
+        <h1 className="text-4xl font-bold m-4">Overflow Voting</h1>
         <Popover>
           <PopoverTrigger className="absolute top-4 right-4 max-w-sm" asChild>
             <div>
@@ -492,107 +240,25 @@ export default function Page() {
             </Card>
           </PopoverContent>
         </Popover>
-        <div className="flex flex-col items-center sm:flex-row gap-4 sm:items-start">
-          <Card className="max-w-xs">
-            <CardHeader>
-              <CardTitle>Sponsored Transaction Example</CardTitle>
-              <CardDescription>
-                This transaction will be sponsored by Enoki and will not require
-                you to pay gas! Try incrementing the counter with a balance of 0
-                SUI to test it out.
-              </CardDescription>
-            </CardHeader>
-            <CardContent className="flex flex-col gap-2">
-              <div className="flex flex-row items-center gap-2">
-                <span>Counter: </span>
-                {countLoading ? (
-                  <LoaderCircle className="animate-spin" />
-                ) : (
-                  <span>{counter}</span>
-                )}
-              </div>
-            </CardContent>
-            <CardFooter className="w-full flex flex-row items-center justify-center">
-              <Button
-                onClick={incrementCounter}
-                disabled={counterLoading}
-                className="w-full"
-              >
-                Increment counter
-              </Button>
-            </CardFooter>
-          </Card>
-
-          <Card className="max-w-xs">
-            <CardHeader>
-              <CardTitle>Transfer Transaction Example</CardTitle>
-              <CardDescription>
-                Transfer SUI to another account. This transaction is not
-                sponsored by the app.
-              </CardDescription>
-            </CardHeader>
-            <CardContent className="flex flex-col w-full gap-2">
-              <div className="grid w-full max-w-sm items-center gap-1.5">
-                <Label htmlFor="recipient">Recipient Address</Label>
-                <Input
-                  type="text"
-                  id="recipient"
-                  placeholder="0xdeadbeef"
-                  value={recipientAddress}
-                  onChange={(e) => setRecipientAddress(e.target.value)}
-                />
-              </div>
-              <div className="grid w-full max-w-sm items-center gap-1.5">
-                <Label htmlFor="amount">Transfer Amount (SUI)</Label>
-                <Input
-                  type="text"
-                  id="amount"
-                  placeholder="1.4"
-                  value={amount}
-                  onChange={(e) => setAmount(e.target.value as any)}
-                />
-              </div>
-            </CardContent>
-            <CardFooter className="w-full flex flex-row items-center justify-center">
-              <Button
-                className="w-full"
-                onClick={transferSui}
-                disabled={transferLoading}
-              >
-                Transfer SUI
-              </Button>
-            </CardFooter>
-          </Card>
-
-          <Card className="max-w-xs">
-            <CardHeader>
-              <CardTitle>Burn SUI</CardTitle>
-              <CardDescription>
-                Burn your SUI.
-              </CardDescription>
-            </CardHeader>
-            <CardContent className="flex flex-col w-full gap-2">
-              <div className="grid w-full max-w-sm items-center gap-1.5">
-                <Label htmlFor="amount">Transfer Amount (SUI)</Label>
-                <Input
-                  type="text"
-                  id="amount"
-                  placeholder="1.4"
-                  value={burnAmount}
-                  onChange={(e) => setBurnAmount(e.target.value as any)}
-                />
-              </div>
-            </CardContent>
-            <CardFooter className="w-full flex flex-row items-center justify-center">
-              <Button
-                className="w-full"
-                onClick={burnSui}
-                disabled={burnLoading}
-              >
-                Transfer SUI
-              </Button>
-            </CardFooter>
-          </Card>
+        <div className="flex flex-wrap justify-center gap-4">
+          {
+            dummyProjects.map((project, index) => {
+              return (
+                <Card key={index} className="w-md">
+                  <CardHeader>
+                    <CardTitle>{project.name}</CardTitle>
+                    <CardDescription>{project.description}</CardDescription>
+                  </CardHeader>
+                  <CardContent>
+                    <div className="flex flex-row items-center justify-between">
+                      <span>Votes: {project.votes}</span>
+                      <Button variant={"outline"} size={"sm"}>Vote</Button>
+                    </div>
+                  </CardContent>
+                </Card>
+              );
+            })
+          }
         </div>
       </div>
     );
@@ -613,18 +279,10 @@ export default function Page() {
         </Button>
       </a>
       <div>
-        <h1 className="text-4xl font-bold m-4">Enoki Demo App</h1>
+        <h1 className="text-4xl font-bold m-4">Overflow Voting</h1>
         <p className="text-md m-4 opacity-50 max-w-md">
-          This is a demo app that showcases the{" "}
-          <a
-            href="https://portal.enoki.mystenlabs.com"
-            target="_blank"
-            className="underline cursor-pointer text-blue-700 hover:text-blue-500"
-          >
-            Enoki
-          </a>{" "}
-          zkLogin flow and sponsored transaction flow. NOTE: This example runs
-          on the <span className="text-blue-700">Sui test network</span>
+          Use this app to vote for your favorite project in the Overflow hackathon!
+          Votes are stored on the Sui network and can be viewed on this page.
         </p>
       </div>
       <Button onClick={startLogin}>Sign in with Google</Button>
