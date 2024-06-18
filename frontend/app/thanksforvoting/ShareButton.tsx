e9ef570da8a92e589f2bf77804ff6bb247134a5e
Daniel Leavitt <daniel.7039@hotmail.com>
2024-06-18 12:24:39 -0700

    fixed build issue

--- frontend/app/thanksforvoting/ShareButton.tsx
+++ frontend/app/thanksforvoting/ShareButton.tsx
@@ -2,19 +2,23 @@
 
 import { Button } from "@/components/ui/button";
 import { IconBrandX } from "@tabler/icons-react";
+import { useEffect, useState } from "react";
 
 export default function ShareButton() {
-  const projectList = localStorage.getItem("votedProjects")?.split(";;") || [];
 
-  console.log("projectList", projectList);
+  const [link, setLink] = useState("");
 
-  const projectLinkInsert = projectList
-    .map((project, index) => {
-      return `%0A-%20${project}`;
-    })
-    .join("");
-
-  const link = `https://twitter.com/intent/tweet?text=I%20just%20voted%20for%20my%20favorite%20Overflow%20projects%21%0A${projectLinkInsert}%0A%0AGo%20vote%20at%3A%20https%3A%2F%2Foverflow-voting-app-git-main-sui-foundation.vercel.app%2F`;
+  useEffect(() => {
+    const projectList = localStorage.getItem("votedProjects")?.split(";;") || [];
+    console.log("projectList", projectList);
+    const projectLinkInsert = projectList
+      .map((project, index) => {
+        return `%0A-%20${project}`;
+      })
+      .join("");
+    const link = `https://twitter.com/intent/tweet?text=I%20just%20voted%20for%20my%20favorite%20Overflow%20projects%21%0A${projectLinkInsert}%0A%0AGo%20vote%20at%3A%20https%3A%2F%2Foverflow-voting-app-git-main-sui-foundation.vercel.app%2F`;
+    setLink(link);
+  }, []);
 
   return (
     <a href={link} target="_blank">
